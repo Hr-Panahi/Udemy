8f419dab49e49e12718b46a1d21663d8d6986c89
Hamid Reza Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2023-10-23 19:49:09 +0330

    Parsing Game Part 2
    Assignment 3:
    Parsing Game (part 2)
    Regular Expressions

---
+++ Parsing_Game_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Parsing_Game_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string readingAllText = File.ReadAllText
+                (@"E:\CS_Internship\Udemy\Parsing_Game_2\input2.txt");
+
+            string pattern = @"\d{2,3}";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchCollection = regex.Matches(readingAllText);
+
+            foreach(Match m in matchCollection)
+            {
+                GroupCollection group = m.Groups;
+                var myValue = int.Parse(group[0].Value);
+                Console.Write((char)myValue);
+            }
+
+        }
+    }
+}
+
+/*
+Subject:
+
+Read and store the whole text from the source file;
+
+Using regular expressions find all sequences of numbers that has length 2 or 3;
+
+Pars every value into an integer;
+
+Use casting to convert every integer into a char. It will look like that:
+(char)YourValue;
+
+and just print the result of each cast on the new line;
+*/
