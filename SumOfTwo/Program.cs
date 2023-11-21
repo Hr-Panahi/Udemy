f43710108e2c3f51be613987573dc428cab44b61
Hamid Reza Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2023-11-21 15:02:23 +0330

    Udemy Section 20: Sum of Two
    This time you have to write a method that will take two arguments: a list of integers nums and an integer SumToFind. And it returns the number of possible UNIQUE pares made from this list where the sum equals to SumToFind

---
+++ SumOfTwo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sampleArray1 = { 2, 3, 6, 7, 15, 6, 0, 21, 9, 1, 2, 3, 4 };
+            Console.WriteLine(SumOfTwo(sampleArray1, 17));
+
+            int[] sampleArray2 = { 3, 14, 9, 7, 15, 11, 6, 0, 21, 9, 1, 5, 3, 4 };
+            Console.WriteLine(SumOfTwo(sampleArray2, 4));
+        }   
+        public static int SumOfTwo(int[] nums, int SumToFind)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int result = 0;
+
+            foreach (int value in nums)
+            {
+                if (dic.ContainsKey(SumToFind - value) && dic[SumToFind - value] > 0)
+                {
+                    dic[SumToFind - value] -= 1;
+                    result++;
+                    continue;
+                }
+                if (dic.ContainsKey(value))
+                    dic[value] += 1;
+
+                else
+                    dic.Add(value, 1);
+            }
+            return result;
+        }
+    }
+}
